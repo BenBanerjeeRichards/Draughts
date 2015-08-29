382b367daf6fa061d0c2960e6f33067c693667ff
Ben Banerjee Richards <benjamin.mced@gmail.com>
2015-08-29 20:42:07 +0100

    Added proper event handing so graphic updates occur in the correct order.

--- BoardState.py
+++ BoardState.py
@@ -1,9 +1,19 @@
+import GameEvent
+
+
+class InvalidGameEventError(Exception):
+	def __init__(self):
+		pass
+
+
 class BoardState:
 
 	def __init__(self, board):
 		self._board = board
 
 		self.checkers = []
+		self._game_event_backlog = []
+		self.event_running = False
 
 	@property
 	def checkers(self):
@@ -12,20 +22,53 @@ class BoardState:
 	def add_checker(self, checker):
 		self.checkers.append(checker)
 
-	def remove_checker(self, location):
-		index = self._get_checker_index_from_location(location)
-		del self.checkers[index]
+	# FIXME change method name
+	def event_completed(self):
+		self.event_running = False
+
+		if len(self._game_event_backlog) > 0:
+			event = self._game_event_backlog[0]
+			print "Running " + str(event)
+			del self._game_event_backlog[0]
+
+			if not event:
+				raise Exception("ERROR ERROR ERROR ")
+			self._run_event(event)
+
+	def _run_event(self, event):
+		if not event.delete_pos:
+			checker = self.get_checker_from_location(event.start)
+			checker.animation_done_callback = self.event_completed
+			print checker.animation_done_callback
+			if not event.start or not event.end:
+				raise InvalidGameEventError("Event must be a delete event or move event")
+
+			checker = self.get_checker_from_location(event.start)
+			checker.animate_move(event.end)
+			self.event_running = True
+
+		else:
+			index = self._get_checker_index_from_location(event.delete_pos)
+			del self.checkers[index]
+			self.event_completed()
+
 
 	def move_checker(self, start_location, end_location):
-		checker = self.get_checker_from_location(start_location)
-		checker.animate_move(end_location)
+		event = GameEvent.GameEvent()
+		event.move(start_location, end_location)
+		self._game_event_backlog.append(event)
 
-	# TODO give this a better name
-	def move_and_remove_checker(self, start_location, end_location, delete_location):
-		checker = self.get_checker_from_location(start_location)
-		checker.animation_done_callback = self.remove_checker
-		checker.remove_checker_location = delete_location
-		checker.animate_move(end_location)
+		if not self.event_running:
+			self.event_completed()
+
+
+	def remove_checker(self, location):
+		event = GameEvent.GameEvent()
+		event.delete(location)
+		self._game_event_backlog.append(event)
+		if not self.event_running:
+			self.event_completed()
+
 
 	def get_team_checkers(self, team):
 		team_checkers = []
